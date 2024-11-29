5ae3c8d9d376d7c81351be2ac9aec7a4e6e41497
Greg Johnston <gregmj.sco@gmail.com>
2024-11-29 13:29:20 +0900

    Add padding to top of header and resize logo image to be a square. Set flex on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,29 @@ import { Container } from "./styles/Container.styled";
 export default function Header() {
   return (
     <StyledHeader>
-      <Container>
-        <img
-          src="../../public/BALogoMockWhite.png"
-          alt="Ball Again logo in white"
-          width="758"
-          height="714"
-          className="header__logo"
-        />
-        <button className="header__bars" aria-label="menu button">
-          <svg
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-        <h1>Header H1</h1>
-      </Container>
+      {/* <Container>  */}
+      <img
+        src="../../public/BALogoMockWhite.png"
+        alt="Ball Again logo in white"
+        width="758"
+        height="714"
+        className="header__logo"
+      />
+      <button className="header__bars" aria-label="menu button">
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      {/* </Container> */}
     </StyledHeader>
   );
 }
--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -3,9 +3,15 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.header};
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+
   .header__logo {
     width: ${({ theme }) => theme.sizes.sevenxl};
-    height: auto;
+    height: ${({ theme }) => theme.sizes.sevenxl};
+    object-fit: cover;
   }
 
   .header__bars {
@@ -13,4 +19,26 @@ export const StyledHeader = styled.header`
     width: ${({ theme }) => theme.sizes.twoxl};
     height: ${({ theme }) => theme.sizes.twoxl};
   }
+
+  /* xs */
+  /* @media (min-width: 475px) {} */
+
+  /* sm */
+  /* @media (min-width: 640px) {} */
+
+  /* md */
+  @media (min-width: 768px) {
+    .header__bars {
+      display: none;
+    }
+  }
+
+  /* lg */
+  /* @media (min-width: 1024px) {} */
+
+  /* xl */
+  /* @media (min-width: 1280px) {} */
+
+  /* 2xl */
+  /* @media (min-width: 1536px) {} */
 `;
