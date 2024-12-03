c7cdf7a6ee6f9740c6243053d17a0d5aebe135f6
Greg Johnston <gregmj.sco@gmail.com>
2024-12-03 13:50:10 +0900

    Fix size of hero image, style image and title. Add marign between image and title

--- src/components/styles/Hero.styled.jsx
+++ src/components/styles/Hero.styled.jsx
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
 export const StyledHero = styled.section`
-  background-color: #f1f1f1;
   text-align: center;
+  margin-top: 2rem;
+
+  .hero > * + * {
+    margin-top: 1rem;
+  }
+
+  .hero__img {
+    width: 100%;
+    height: 70vh;
+    object-fit: contain;
+    margin-inline: auto;
+  }
+
+  .hero__title {
+    font-size: ${({ theme }) => theme.sizes.threexl};
+    font-weight: 900;
+    line-height: 1;
+  }
 `;
