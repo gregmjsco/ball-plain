aab2edaf6b0c0699ab54c3d0de0bca121ead2aa0
Greg Johnston <gregmj.sco@gmail.com>
2024-12-04 15:38:29 +0900

    Fix layout of products component with flex and tweak hover effect to have translateZ effect

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,34 +5,34 @@ export default function Products() {
     <StyledProducts>
       <div className="list">
         <div className="item">
-          <img src="/1.png" alt=""></img>
+          <img src="/1.png" alt="" width="998" height="666"></img>
         </div>
         <div className="item">
-          <img src="/2.png" alt=""></img>
+          <img src="/2.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/3.png" alt=""></img>
+          <img src="/3.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/4.png" alt=""></img>
+          <img src="/4.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/5.png" alt=""></img>
+          <img src="/5.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/6.png" alt=""></img>
+          <img src="/6.png" alt="" width="997" height="666"></img>
         </div>
         <div className="item">
-          <img src="/1.png" alt=""></img>
+          <img src="/1.png" alt="" width="998" height="666"></img>
         </div>
         <div className="item">
-          <img src="/2.png" alt=""></img>
+          <img src="/2.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/3.png" alt=""></img>
+          <img src="/3.png" alt="" width="997" height="667"></img>
         </div>
         <div className="item">
-          <img src="/4.png" alt=""></img>
+          <img src="/4.png" alt="" width="997" height="667"></img>
         </div>
       </div>
     </StyledProducts>
--- src/components/styles/Products.styled.jsx
+++ src/components/styles/Products.styled.jsx
@@ -1,12 +1,32 @@
 import styled from "styled-components";
 
 export const StyledProducts = styled.div`
+  img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+
+    width: 100%;
+    height: 80vh;
+
+    transform-style: preserve-3d;
+    transform: perspective(10000px);
+  }
+
   .list .item {
     transition: 0.5s;
-    filter: brightness(0.1);
+    filter: brightness(0.8);
   }
 
   .list .item:hover {
     filter: brightness(1);
+    transform: translateZ(200px);
   }
 `;
