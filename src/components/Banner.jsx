d3f6ff2c733db508ac3467020de70fcc2874646a
Greg Johnston <gregmj.sco@gmail.com>
2024-12-09 11:34:04 +0900

    Add slider container and placeholder images. Set only first child of slider to be displayed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,13 @@
 import { StyledBanner } from "./styles/Banner.styled";
 
 export default function Banner() {
-  return <StyledBanner></StyledBanner>;
+  return (
+    <StyledBanner>
+      <div className="slider">
+        <img src="/1.png" alt="" width="998" height="666" />
+        <img src="/2.png" alt="" width="997" height="667" />
+        <img src="/3.png" alt="" width="997" height="667" />
+      </div>
+    </StyledBanner>
+  );
 }
--- src/components/styles/Banner.styled.jsx
+++ src/components/styles/Banner.styled.jsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const StyledBanner = styled.div``;
+export const StyledBanner = styled.div`
+  .slider img:not(img:first-child) {
+    display: none;
+  }
+`;
