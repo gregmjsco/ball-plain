bb7d7f9dbe61f74744f9898d45a7300d4eaa40ec
Greg Johnston <gregmj.sco@gmail.com>
2024-12-14 19:28:30 +0900

    Fix slider button by adding missing ref prop to slider div

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,34 +5,19 @@ export default function Banner() {
   const sliderRef = useRef(null);
   const nextBtnRef = useRef(null);
 
-  //   useEffect(() => {
-  //     let nextBtn = document.getElementById("next");
-  //     let sliderRef = document.getElementById("slider");
-
-  //     if (nextBtn) {
-  //       nextBtn.addEventListener("click", handleNext);
-  //     }
-
-  //     return () => {
-  //       if (nextBtn) {
-  //         nextBtn.removeEventListener("click", handleNext);
-  //       }
-  //     };
-  //   }, []);
-
   const handleNext = () => {
     console.log("Next button clicked");
     if (sliderRef.current) {
       const firstImage = sliderRef.current.querySelector("img:first-child");
       console.log("First image:", firstImage);
-      sliderRef.current.appendChild(firstImage);
+      sliderRef.current.append(firstImage);
       console.log("Slider children:", sliderRef.current.children);
     }
   };
 
   return (
     <StyledBanner>
-      <div className="slider">
+      <div className="slider" ref={sliderRef}>
         <img src="/1.png" alt="" width="998" height="666" />
         <img src="/2.png" alt="" width="997" height="667" />
         <img src="/3.png" alt="" width="997" height="667" />
