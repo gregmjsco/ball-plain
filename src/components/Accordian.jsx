4ef2045d944d3d3266b9ad8cd747e76c3d0f17dc
Greg Johnston <gregmj.sco@gmail.com>
2024-12-30 17:23:31 +0900

    Add single selection functionality to accordian component

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,5 +1,33 @@
 import { StyledAccordian } from "./styles/Accordian.styled";
+import { useState } from "react";
+import data from "./data";
 
 export default function Accordian() {
-  return <StyledAccordian>Accordian Component</StyledAccordian>;
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId);
+  }
+  return (
+    <StyledAccordian>
+      {data && data.length > 0 ? (
+        data.map((dataItem) => (
+          <div key={dataItem.id} className="item">
+            <div
+              onClick={() => handleSingleSelection(dataItem.id)}
+              className="title"
+            >
+              <h3>{dataItem.title}</h3>
+              <span>+</span>
+            </div>
+            {selected === dataItem.id ? (
+              <div className="content">{dataItem.text}</div>
+            ) : null}
+          </div>
+        ))
+      ) : (
+        <div>No data found</div>
+      )}
+    </StyledAccordian>
+  );
 }
---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: "1",
+    title: "What's Ball Again?",
+    text: "Ball Again is a basketball streetwear brand from Tokyo",
+  },
+  {
+    id: "2",
+    title: "How long is shipping?",
+    text: "Shipping times vary based on location but in general within 1 week of order",
+  },
+];
+
+export default data;
