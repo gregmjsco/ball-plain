6a1a5147ab7774097238c7314d2283cfc922d7ad
Greg Johnston <gregmj.sco@gmail.com>
2025-01-01 16:59:47 +0900

    Add some placeholder styling to accordian component

--- src/components/styles/Accordian.styled.jsx
+++ src/components/styles/Accordian.styled.jsx
@@ -1,3 +1,25 @@
 import styled from "styled-components";
 
-export const StyledAccordian = styled.div``;
+export const StyledAccordian = styled.div`
+  .title {
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--cln-dark);
+    color: var(--clr-text);
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    text-align: left;
+    border: none;
+    outline: none;
+    transition: 0.4s;
+    margin-bottom: 5px;
+  }
+
+  .content {
+    padding: 0 18px;
+    color: var(--clr-text);
+    background-color: var(--cln-dark);
+    overflow: hidden;
+  }
+`;
