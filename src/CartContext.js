88f7c43aba59e1d565c6b21fa6d2892da5f8deb7
Greg Johnston <gregmj.sco@gmail.com>
2025-01-02 16:03:55 +0900

    Create CartContext with some boilerplate. Need to add product data to seperate file

---
+++ src/CartContext.js
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+const CartContext = createContext({
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
+});
