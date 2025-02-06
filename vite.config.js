78d4ed495f8365dbf8d9749785477a4503e88356
Greg Johnston <gregmj.sco@gmail.com>
2025-02-06 14:29:18 +0900

    Update config settings

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ball-plain/",
-  build: {
-    outDir: "dist", // The default build output directory
-  },
+  base: "/ball-plain",
 });
